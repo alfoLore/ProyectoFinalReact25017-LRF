e53af8cfb7958a63c511ba50689a8785d3da1d9a
LOrena Febbraro <febbweb@gmail.com>
2025-05-16 16:11:22 -0300

    Inicio de los .jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React form 'React';
+import ProductList from './ProductList';
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h1>Todos los productos</h1>
+            {/* <ProductList/> */}
+         </div>
+    );
+};
